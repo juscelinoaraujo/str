3714a41a92dea411d6d952624711572cadd3bda3
Juscelino Pereira de Araújo <juscelino.caico@gmail.com>
2024-03-31 20:36:18 -0300

    Add files via upload

--- tempo_reacao4.py
+++ tempo_reacao4.py
@@ -13,6 +13,53 @@ score = 0
 start_time = 0
 total_time = 0
 
+def show_text(state):
+  global score, total_time
+  font1 = pygame.font.SysFont('monospace', 27)
+  font2 = pygame.font.SysFont('monospace', 20)
+  if state == 0: 
+    title = "Teste de tempo de reação"
+    inst1 = "Nas bolas azuis, aperte R"
+    inst2 = "Nas bolas verdes, aperte T"
+    inst3 = "Nas bolas amarelas, aperte Y"
+    inst4 = "Nas bolas vvermelhas, aperte U"
+    note = "Para iniciar o teste, aperte W"
+    text_title = font1.render(title, True, "black", "white")
+    text_inst1 = font2.render(inst1, True, "black", "white")
+    text_inst2 = font2.render(inst2, True, "black", "white")
+    text_inst3 = font2.render(inst3, True, "black", "white")
+    text_inst4 = font2.render(inst4, True, "black", "white")
+    text_note = font1.render(note, True, "white", "black")
+    screen.blit(text_title, (205, 100))
+    screen.blit(text_inst1, (205, 200))
+    screen.blit(text_inst2, (205, 250))
+    screen.blit(text_inst3, (205, 300))
+    screen.blit(text_inst4, (205, 350))	
+    screen.blit(text_note, (150, 500))
+  elif state == 1:
+    note = "Prepare-se"
+    text_note = font1.render(note, True, "white", "black")
+    screen.blit(text_note, (325, 250))
+  elif (state == 2):
+    note = "Para parar o teste, aperte Q"
+    text_note = font1.render(note, True, "white", "black")
+    screen.blit(text_note, (175, 500))
+  elif (state == 4):
+    show_score = "Score: " + str(score) + "/20"
+    text_score = font1.render(show_score, True, "white", "black")
+    note = "Para reiniciar o teste, aperte W"
+    if score != 0:
+      average = round(0.001*total_time/score, 3)
+    else:
+      average = 0
+    show_average = "Média de tempo de reação: " + str(average) + "s"
+    text_score = font1.render(show_score, True, "white", "black")
+    text_average = font1.render(show_average, True, "white", "black")
+    text_note = font1.render(note, True, "white", "black")
+    screen.blit(text_score, (150, 200))
+    screen.blit(text_average, (150, 250))
+    screen.blit(text_note, (150, 500))
+
 def show_time(step):
 	return 2000 - 50*step
 
@@ -57,7 +104,8 @@ def test_action(start, target):
 def start_screen():
   global state
   screen.fill("black")
-  pygame.draw.rect(screen, "white", (200, 100, 400, 300))
+  center_rect = pygame.draw.rect(screen, "white", (200, 100, 400, 300))
+  show_text(state)
   pygame.display.flip()
   keys = pygame.key.get_pressed()
   if keys[pygame.K_w]:
@@ -69,6 +117,7 @@ def test_start():
   score = 0
   total_time = 0
   screen.fill("black")
+  show_text(state)
   pygame.display.flip()
   pygame.time.delay(2000)
   state = 2
@@ -77,6 +126,7 @@ def test_screen():
   global state, target, start_time, reacted
   screen.fill("black")
   reacted = False
+  show_text(state)
   target = draw_circle()
   pygame.display.flip()
   start_time = pygame.time.get_ticks()
@@ -86,6 +136,7 @@ def test_screen():
 def end_screen():
   global state, score, total_time
   screen.fill("black")
+  show_text(state)
   pygame.display.flip()
   keys = pygame.key.get_pressed()
   if keys[pygame.K_w]:
